d6f3b2b49d046a74a388527fd4347b8050f8d61d
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-08-18 10:20:28 -0300

    feat: documenting auth and user routes

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,17 +2,42 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthRegisterDto } from './dto/auth-register.dto';
 import { AuthLoginDto } from './dto/auth-login.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtDto } from './dto/jwt.dto';
+import { UserAlredyExistsDto } from 'src/user/dto/user-exists.dto';
+import { UnauthorizedLoginDto } from './dto/unauthorized-login.dto';
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiResponse({
+    status: 201,
+    description: 'Created',
+    type: JwtDto,
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Conflict',
+    type: UserAlredyExistsDto,
+  })
   async register(@Body() body: AuthRegisterDto) {
     return this.authService.register(body);
   }
 
   @Post('login')
+  @ApiResponse({
+    status: 200,
+    description: 'Login',
+    type: JwtDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+    type: UnauthorizedLoginDto,
+  })
   async login(@Body() { email, password }: AuthLoginDto) {
     return await this.authService.login(email, password);
   }
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -17,38 +17,90 @@ import { User } from 'src/decorators/user.decorator';
 import { Roles } from 'src/decorators/role.decorator';
 import { Role } from 'src/enuns/role.enum';
 import { RoleGuard } from 'src/guards/role.guard';
-
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiSecurity,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ForbiddenErrorDto } from './dto/forbidden-error.dto';
+import { UserAlredyExistsDto } from './dto/user-exists.dto';
+import { UserdNotFoundDto } from './dto/userid-notFond.dto';
 @Controller('user')
 @Roles(Role.Admin)
 @UseGuards(AuthGuard, RoleGuard)
+@ApiTags('User')
+@ApiBearerAuth()
+@ApiSecurity('bearer')
+@ApiResponse({
+  status: 403,
+  description: 'Forbidden.',
+  type: ForbiddenErrorDto,
+})
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({ summary: 'Create User' })
+  @ApiResponse({
+    status: 201,
+    description: 'user created successfully',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Conflict',
+    type: UserAlredyExistsDto,
+  })
   @Post('create')
   async create(@Body() body: CreateUserDto) {
     return this.userService.create(body);
   }
 
+  @ApiOperation({ summary: 'Update partial' })
+  @ApiResponse({
+    status: 409,
+    description: 'Conflict',
+    type: UserAlredyExistsDto,
+  })
   @Patch('update/:id')
   async update(@Body() body: UpdateUserDto, @Param('id') id: string) {
     return this.userService.update(body, id);
   }
 
+  @ApiResponse({
+    status: 404,
+    description: 'Not Found',
+    type: UserdNotFoundDto,
+  })
+  @ApiOperation({ summary: 'User search one' })
   @Get('readOne/:id')
   async readOne(@Param('id') id: string) {
     return this.userService.readOne(id);
   }
 
+  @ApiOperation({ summary: 'Users search all' })
   @Get('readAll')
   async readAll() {
     return this.userService.readAll();
   }
 
+  @ApiResponse({
+    status: 404,
+    description: 'Not Found',
+    type: UserdNotFoundDto,
+  })
+  @ApiOperation({ summary: 'Delete user' })
   @Delete('delete/:id')
   async delete(@Param('id') id: string) {
     return this.userService.delete(id);
   }
 
+  @ApiResponse({
+    status: 404,
+    description: 'Not Found',
+    type: UserdNotFoundDto,
+  })
+  @ApiOperation({ summary: 'Returns user based on token' })
   @Post('me')
   async me(@User() user: UserEntity) {
     return user;

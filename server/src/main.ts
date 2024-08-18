469ac8df653ce41c419539a102c1d1dc459b26b2
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-08-18 10:18:22 -0300

    feat: initial settings for using swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Amor-Saúde')
+    .setDescription('Documentação para o projeto proposto pelo Felipe')
+    .setVersion('1.0')
+    .addTag('Documentação')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
 

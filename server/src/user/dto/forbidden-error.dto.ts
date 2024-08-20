4465170b76aaf6f3563ab1385da53ece3ce55680
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-08-19 23:47:02 -0300

    refactor: fixing dto of ForbiddenErrorDto

--- server/src/user/dto/forbidden-error.dto.ts
+++ server/src/user/dto/forbidden-error.dto.ts
@@ -4,9 +4,16 @@ export class ForbiddenErrorDto {
   @ApiProperty({ example: 409 })
   statusCode: number;
 
-  @ApiProperty({ example: 'Forbidden resource' })
+  @ApiProperty({
+    example: 'Forbidden resource',
+  })
   message: string;
 
-  @ApiProperty({ example: 'Forbidden.' })
-  error: string;
+  @ApiProperty({ example: '2024-08-20T02:32:19.108Z' })
+  timestamp: Date;
+
+  @ApiProperty({
+    example: '/user/readOne/87accf20-1940-4d46-833e-55299845da64',
+  })
+  path: string;
 }

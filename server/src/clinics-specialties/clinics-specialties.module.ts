bf5528996da41e6532190a3166b0875d45bd5f7b
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-08-21 11:34:53 -0300

    fis: Importing modules for use in ClinicsSpecialtiesModule

--- server/src/clinics-specialties/clinics-specialties.module.ts
+++ server/src/clinics-specialties/clinics-specialties.module.ts
@@ -5,12 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClinicsSpecialtiesEntity } from './entities/clinics-specialties.entity';
 import { ClinicsModule } from 'src/clinics/clinics.module';
 import { SpecialtiesModule } from 'src/specialties/specialties.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ClinicsSpecialtiesEntity]),
     ClinicsModule,
     SpecialtiesModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [ClinicsSpecialtiesController],
   providers: [ClinicsSpecialtiesServices],

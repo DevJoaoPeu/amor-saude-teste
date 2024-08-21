896a3a7c8268f169d52fbd75255dfe755460eb5a
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-08-20 22:07:41 -0300

    refactor: adjusting property names in dto

--- server/src/clinics-specialties/dto/create-clinics-specialties.dto.ts
+++ server/src/clinics-specialties/dto/create-clinics-specialties.dto.ts
@@ -4,9 +4,9 @@ import { IsUUID } from 'class-validator';
 export class CreateClinicsSpecialtiesDto {
   @IsUUID()
   @ApiProperty({ description: 'ID to specialties' })
-  specialty_id: string;
+  specialty: string;
 
   @IsUUID()
   @ApiProperty({ description: 'ID to clinics' })
-  clinics_id: string;
+  clinic: string;
 }

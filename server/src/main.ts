ad9cfff761e536bcf00bf426344e11ffefbbcae8
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-08-21 14:11:05 -0300

    feat: enabled cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Amor-Saúde')
     .setDescription('Documentação para o projeto proposto pelo Felipe')

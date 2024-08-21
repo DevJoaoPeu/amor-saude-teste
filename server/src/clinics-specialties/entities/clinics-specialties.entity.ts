e4fc0e1042bc43ea74d6b1a8706279a0cec54d7b
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-08-20 21:21:17 -0300

    fix: correcting table name

--- server/src/clinics-specialties/entities/clinics-specialties.entity.ts
+++ server/src/clinics-specialties/entities/clinics-specialties.entity.ts
@@ -2,7 +2,7 @@ import { ClinicEntity } from 'src/clinics/entities/clinics.entity';
 import { SpecialtiesEntity } from 'src/specialties/entitites/specialties.entity.dto';
 import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('clinics-specialties')
+@Entity('clinics_specialties')
 export class ClinicsSpecialtiesEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;

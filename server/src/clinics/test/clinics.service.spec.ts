1eddb37c8cbc34fd715edb1c392d11feb2ebb718
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-12-15 10:44:44 -0300

    test: testing method created

--- server/src/clinics/test/clinics.service.spec.ts
+++ server/src/clinics/test/clinics.service.spec.ts
@@ -5,6 +5,8 @@ import { ClinicsService } from "../clinics.service";
 import { Repository } from "typeorm";
 import { ClinicEntity } from "../entities/clinics.entity";
 import { getRepositoryToken } from "@nestjs/typeorm";
+import { create } from "domain";
+import { CreateClinicDto } from "../dto/create-clinics.dto";
 
 describe('', () => {
     let clinicsService: IClinicsService;
@@ -16,6 +18,9 @@ describe('', () => {
             find: jest.fn(),
             save: jest.fn(),
             findOne: jest.fn(),
+            create: jest.fn(),
+            delete: jest.fn(),
+            update: jest.fn()
         };
 
         let module: TestingModule = await Test.createTestingModule({
@@ -27,7 +32,7 @@ describe('', () => {
                 {
                     provide: IRegionalService,
                     useValue: {
-                        regionalService: jest.fn()
+                        regionalAlredyExists: jest.fn()
                     }
                 },
                 {
@@ -46,5 +51,51 @@ describe('', () => {
         expect(clinicsService).toBeDefined();
         expect(regionalService).toBeDefined();
         expect(repository).toBeDefined();
+    })
+
+    describe('create', () => {
+        it('deve criar uma clínica', async () => {
+            const mockClinic: CreateClinicDto = {
+                razaoSocial: 'razao social',
+                nomeFantasia: 'nome fantasia',
+                cnpj: '09.512.501/0001-21',
+                dataInauguracao: '2024-08-15',
+                ativa: true,
+                regional: '123',
+            } 
+
+            const mockRegional = {
+                id: '123',
+                name: 'Regional'
+            }
+
+            const mockClinicEntity: ClinicEntity = {
+                id: '1',
+                razaoSocial: mockClinic.razaoSocial,
+                nomeFantasia: mockClinic.nomeFantasia,
+                cnpj: '09.512.501/0001-21', 
+                dataInauguracao: new Date(mockClinic.dataInauguracao),
+                ativa: mockClinic.ativa,
+                regional: mockRegional,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            };
+
+            jest.spyOn(regionalService, 'regionalAlredyExists').mockResolvedValue(mockRegional);
+            jest.spyOn(repository, 'create').mockReturnValue(mockClinicEntity);
+            jest.spyOn(repository, 'save').mockResolvedValue(mockClinicEntity);
+
+            const clinic = await clinicsService.create(mockClinic);
+    
+            expect(repository.create).toHaveBeenCalledWith({
+                ...mockClinic,
+                regional: mockRegional,
+            });
+            expect(repository.save).toHaveBeenCalled();
+            expect(clinic).toEqual({
+                ...mockClinicEntity,
+                dataInauguracao: new Date(mockClinic.dataInauguracao),
+            });
+        })
     })
 })
